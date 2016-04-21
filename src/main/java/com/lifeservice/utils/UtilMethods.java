3d8ffb5a9423c8a0b98813657318cb901bb11434
liyi <liyi@liyi-PC>
2016-04-21 21:48:05 +0800

    update

--- src/main/java/com/lifeservice/utils/UtilMethods.java
+++ src/main/java/com/lifeservice/utils/UtilMethods.java
@@ -187,7 +187,7 @@ public class UtilMethods {
 	 
 	 public static String dqsj() {  //此方法用于获得当前系统时间（格式类型2007-11-6 15:10:58）
 		   Date date = new Date();  //实例化日期类型
-		   String today = date.toLocaleString(); //获取当前时间
+		   String today = date.toString(); //获取当前时间
 		   System.out.println("获得当前系统时间 "+today);  //显示
 		   return today;  //返回当前时间
 		  }
